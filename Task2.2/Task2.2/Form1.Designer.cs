59fb16f339defd2d2a3e12e5a9f59068413cbed3
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-07-26 14:12:52 +0300

    Add files via upload

---
+++ Task2.2/Task2.2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Task2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttons = new System.Windows.Forms.Button[100];
+            for(int i =0; i < 100; i++)
+            {
+                this.buttons[i] = new System.Windows.Forms.Button();
+                
+            }
+
+            this.SuspendLayout();
+
+            // 
+            // button1
+            // 
+
+            textBox.Location = new System.Drawing.Point(410, 2);
+            textBox.Size = new System.Drawing.Size(300, 200);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    string textB = RandomString(new System.Random(i * 10 + j), 1, false);
+                    this.buttons[i*10+j].Location = new System.Drawing.Point(40*i, 40*j);
+                    this.buttons[i*10+j].Name = "button" + i;
+                    this.buttons[i*10+j].Size = new System.Drawing.Size(30, 30);
+                    this.buttons[i*10+j].TabIndex = 0;
+                    this.buttons[i * 10 + j].Text = textB;
+                    this.buttons[i*10+j].UseVisualStyleBackColor = true;
+                    this.buttons[i * 10 + j].Click += new System.EventHandler((sender, e)=>{
+                        this.textBox.Text += textB;
+                        this.Update();
+                    });
+                }
+            }
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            for (int i = 0; i < 100; i++)
+            {
+                this.Controls.Add(this.buttons[i]);
+            }
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button[] buttons;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Task2.2/Task2.2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace Task2._2
+{
+   
+
+
+    public partial class Form1 : Form
+    {
+        private enum UTFBinds
+        {
+            ONE = 31, TWO = 32, THREE = 33, FOUR = 34, FIVE = 35, 
+            SIX = 36, SEVEN = 37, EIGHT = 38, NINE = 39, A = 41, B = 42, 
+            C = 43, D = 44, E = 45, F = 46, G = 47, H = 48, I = 49, J =50,
+            K = 51, L = 52, M = 53, N = 54, O = 55, P = 56,  Q = 57, R = 58, 
+            S = 59, 
+
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public string RandomString(Random random, int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(30 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+    }
+}
