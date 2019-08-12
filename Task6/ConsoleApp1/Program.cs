242517d7af5aee536fc6965c4b9af69270b6bcc0
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-08-12 13:06:56 +0300

    Add files via upload

--- Task6/ConsoleApp1/Program.cs
+++ Task6/ConsoleApp1/Program.cs
@@ -1,60 +1,124 @@
-﻿using System;
+﻿//********************************************************************************
+//* Check App.config and <connectionStrings>                                     *
+//* it has non-defualt configuration connectionString=...                        *
+//* DBConsoleApi class have Fill_DB method which can be invoked once to fill DB  *
+//* Cherkasov Dmitry | Task 6 | ORM | Entity Framework                           *
+//********************************************************************************
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Threading.Tasks;
 
+
 namespace EntityApp
 {
- 
     public class Student
     {
         public int ID { get; set; }
         public int groupID { get; set; }
         public string name { get; set; }
     }
-
-
-
     public class Group
     {
         public int ID { get; set; }
         public string name { get; set; }
     }
-
-
-
     public class StudentContext : DbContext
     {
         public StudentContext(string nameOrConnection) : base("DbConnection1"){}
         public DbSet<Student> Students { get; set; }
         public DbSet<Group> Groups { get; set; }
     }
-    public class Program
-    {
-        private static int StudentCount;
 
-        public Program()
+    public class DBConsoleApi
+    {
+        
+        private static int StudentCount; 
+        private const int GroupCount = 3;
+        public DBConsoleApi()
         {
             StudentCount = 0;
+            using (StudentContext dbSession = new StudentContext("DbConnection"))
+            {
+                foreach (var student in dbSession.Students)
+                {
+                    StudentCount++;
+                }
+            }
         }
-        private static void ViewAllStudentAtGroup(StudentContext db, int GroupID)
+        public int GetStudentCount()
+        {
+            return StudentCount;
+        }
+        private List<int> GetValidStudentsIDAtGroup(StudentContext db, int GroupID)
+        {
+            var result = new List<int>();
+            foreach(var item in db.Students)
+            {
+                if(item.groupID == GroupID)
+                {
+                    result.Add(item.ID);
+                }
+            }
+            return result;
+        }
+        private int ReadIntWithCheckRange(List<int> ValidIndexes)
+        {
+            try
+            {
+                int ReadedInt = int.Parse(Console.ReadLine());
+                if (!ValidIndexes.Contains(ReadedInt))
+                {
+                    Console.WriteLine("Wrong key given...");
+                    return ReadIntWithCheckRange(ValidIndexes);
+                }
+                return ReadedInt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return ReadIntWithCheckRange(ValidIndexes);
+            }
+        }
+        private int ReadIntWithCheckRange(int Range)
+        {
+            try
+            {
+                int ReadedInt = int.Parse(Console.ReadLine());
+                if (ReadedInt < 0 || ReadedInt > Range)
+                {
+                    Console.WriteLine("Wrong key given...");
+                    return ReadIntWithCheckRange(Range);
+                }
+                return ReadedInt;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return ReadIntWithCheckRange(Range);
+            }
+        }
+        public void ViewAllStudentAtGroup(StudentContext db, int GroupID)
         {
             foreach (var student in db.Students)
             {
                 if (student.groupID == GroupID)
                 {
                     Console.WriteLine("Student {0} is {1}", student.ID, student.name);
-                } 
+                }
             }
         }
-        private static void ViewAllStudentsByGroups(StudentContext db)
+        public void ViewAllStudentsByGroups(StudentContext db)
         {
             foreach (var group in db.Groups)
             {
-                Console.WriteLine("\n========================================");
-                Console.WriteLine("Group №{0} is {1}", group.ID, group.name);
+                Console.WriteLine("========================================");
+                Console.WriteLine("Group №{0} is {1}:", group.ID, group.name);
+                Console.WriteLine("========================================");
                 Console.WriteLine();
                 foreach (var student in db.Students)
                 {
@@ -64,50 +128,60 @@ namespace EntityApp
                             student.ID, student.name, group.name);
                     }
                 }
+                Console.WriteLine('\n');
             }
         }
-        private static void ViewAllGroups(StudentContext db)
+        public void ViewAllGroups(StudentContext db)
         {
-            foreach(var group in db.Groups)
+            foreach (var group in db.Groups)
             {
                 Console.WriteLine("Group №{0} is {1}", group.ID, group.name);
             }
         }
-        private static void FillContent(StudentContext db)
+        private static void FillContent()
         {
-            db.Groups.Add( new Group { ID = 1, name = "RadioPhysics" });
-            db.Groups.Add(new Group { ID = 2, name = "MicroElectrics" });
-            db.Groups.Add(new Group { ID = 3, name = "GeneralPhysics" });
-            // TODO ЗАПОЛНЕНИЕ ЦАРЯМИ РОССИИ
-            db.Students.Add(new Student { ID = 1, name = "zxc", groupID = 1 });
-            db.Students.Add(new Student { ID = 2, name = "sdsd", groupID = 3 });
-            db.Students.Add(new Student { ID = 3, name = "asfsdfad", groupID = 1 });
-            db.Students.Add(new Student { ID = 4, name = "er", groupID = 2 });
-            db.Students.Add(new Student { ID = 5, name = "dsfdsf", groupID = 2 });
-            db.Students.Add(new Student { ID = 6, name = "afsdfsafdsf", groupID = 1 });
-            db.Students.Add(new Student { ID = 7, name = "1r32rewf", groupID = 3 });
-            db.Students.Add(new Student { ID = 8, name = "5urtc", groupID = 2 });
-            db.Students.Add(new Student { ID = 9, name = "gdsfghgfh", groupID = 1 });
-            db.Students.Add(new Student { ID = 10, name = "Vcxvc", groupID = 3 });
-            db.Students.Add(new Student { ID = 11, name = "efec", groupID = 1 });
-            db.SaveChanges();
+            using (StudentContext db = new StudentContext("DbConnection"))
+            {
+                db.Groups.Add(new Group { ID = 1, name = "RadioPhysics" });
+                db.Groups.Add(new Group { ID = 2, name = "MicroElectrics" });
+                db.Groups.Add(new Group { ID = 3, name = "GeneralPhysics" });
+                // TODO ЗАПОЛНЕНИЕ ЦАРЯМИ РОССИИ
+                db.Students.Add(new Student { ID = 1, name = "zxc", groupID = 1 });
+                db.Students.Add(new Student { ID = 2, name = "sdsd", groupID = 3 });
+                db.Students.Add(new Student { ID = 3, name = "asfsdfad", groupID = 1 });
+                db.Students.Add(new Student { ID = 4, name = "er", groupID = 2 });
+                db.Students.Add(new Student { ID = 5, name = "dsfdsf", groupID = 2 });
+                db.Students.Add(new Student { ID = 6, name = "afsdfsafdsf", groupID = 1 });
+                db.Students.Add(new Student { ID = 7, name = "1r32rewf", groupID = 3 });
+                db.Students.Add(new Student { ID = 8, name = "5urtc", groupID = 2 });
+                db.Students.Add(new Student { ID = 9, name = "gdsfghgfh", groupID = 1 });
+                db.Students.Add(new Student { ID = 10, name = "Vcxvc", groupID = 3 });
+                db.Students.Add(new Student { ID = 11, name = "efec", groupID = 1 });
+                db.SaveChanges();
+            }
 
         }
-        private static void AddStudentToDB(StudentContext db, string studentName, int GroupID)
+        public void AddStudentToDB(string studentName, int GroupID)
         {
-            db.Students.Add(new Student { name = studentName, groupID = GroupID });
-            db.SaveChanges();
+            using (StudentContext dbSession = new StudentContext("DbConnection"))
+            {
+                dbSession.Students.Add(new Student { name = studentName, groupID = GroupID });
+                dbSession.SaveChanges();
+            }
             StudentCount++;
         }
-        private static void RemoveStudentFromDB(StudentContext db, int StudentID)
+        public void RemoveStudentFromDB(int StudentID)
         {
-            // TODO check ID student should be legal
-            var toDelete = new Student { ID = StudentID};
-            db.Entry(toDelete).State = EntityState.Deleted;
-            db.SaveChanges();
+            using (StudentContext dbSession = new StudentContext("DbConnection"))
+            {
+                var toDelete = new Student { ID = StudentID };
+                dbSession.Students.Attach(toDelete);
+                dbSession.Students.Remove(toDelete);
+                dbSession.SaveChanges();
+            }
             StudentCount--;
         }
-        private static int DoBinaryVote(string message)
+        private int DoBinaryVote(string message)
         {
             Console.WriteLine(message);
             string vote = Console.ReadLine();
@@ -129,53 +203,59 @@ namespace EntityApp
             }
             return 0;
         }
-
-
-
-
-        public static void Main(string[] args)
+        public void StartAskSession()
         {
-            using (StudentContext db = new StudentContext("DbConnection"))
+            using (StudentContext dbView = new StudentContext("DbConnection"))
             {
-                /// 1)вывод студентов
-                /// группа - номер
-                /// студент 1 ФИО
-                /// студент 2 ФИО
-                /// ....
-                /// студент Н фио
-                /// группа - номер 
-                /// ...........
+                /// 1)
                 
-                ViewAllStudentsByGroups(db);
+                ViewAllStudentsByGroups(dbView);
+                // 2) Ask 
+                Console.WriteLine("\n");
 
-
-
-                // 2) вопрос 
-               Console.WriteLine("\n");
-               if( DoBinaryVote
-                    ("Choose one step: \n 1. Add Student (key1)\t\t2. Remove Student (key2)") == 1)
+                if (DoBinaryVote
+                     ("Choose one step: \n 1. Add Student (key1)\t\t2. Remove Student (key2)") == 1)
                 {
                     //3)
-                    Console.WriteLine("\nChoose group ID..");
-                    ViewAllGroups(db);
-                    string groupID = Console.ReadLine();
+                    Console.WriteLine("\nChoose group ID... (1 - {0})", GroupCount);
+                    ViewAllGroups(dbView);
+                    int groupID = ReadIntWithCheckRange(GroupCount);
                     Console.WriteLine("\nInsert student name...");
                     string name = Console.ReadLine();
-                    AddStudentToDB(db, name, int.Parse(groupID));
+                    AddStudentToDB(name, groupID);
+                    Console.WriteLine("Студент " + name + "добавлен..\n");
+                    ViewAllStudentsByGroups(dbView);
                 }
                 else
                 {
                     //4)
-                    Console.WriteLine("\nChoose group ID..");
-                    ViewAllGroups(db);
-                    string groupID = Console.ReadLine();
-                    ViewAllStudentAtGroup(db, int.Parse(groupID));
+                    Console.WriteLine("\nChoose group ID... (1 - {0})", GroupCount);
+                    ViewAllGroups(dbView);
+                    int groupID = ReadIntWithCheckRange(GroupCount);
+                    ViewAllStudentAtGroup(dbView, groupID);
                     Console.WriteLine("\nInsert student ID...");
-                    string studentID = Console.ReadLine();
-
+                    int studentID = ReadIntWithCheckRange(GetValidStudentsIDAtGroup(dbView, groupID));
+                    RemoveStudentFromDB(studentID);
+                    ViewAllStudentsByGroups(dbView);
                 }
-                Console.Read();
-            } 
+            }
+            if (DoBinaryVote
+                      ("\nChoose one step: \n 1. Return to menu (key1)\t\t2. Exit (key2)") == 1)
+            {
+                Console.WriteLine("\n\n\n========================================");
+                StartAskSession();
+            }
+
+        }
+    }
+
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            DBConsoleApi DbSession = new DBConsoleApi();
+            DbSession.StartAskSession();
         }
     }
 }
