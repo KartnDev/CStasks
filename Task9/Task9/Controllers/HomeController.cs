4f513f2e87d37cce84114e9161ab362fbbdbce99
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-08-21 22:46:39 +0300

    Add files via upload

---
+++ Task9/Task9/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Task9.Models;
+
+namespace Task9.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Edit(int studentID, int groupID)
+        {
+            int ID = studentID; int gID = groupID;
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
