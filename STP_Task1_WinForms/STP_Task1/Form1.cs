ec12ecc9b3ecb5640f6ecdc4772d8c33e0461c41
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-09-24 17:26:29 +0300

    Add files via upload

--- STP_Task1_WinForms/STP_Task1/Form1.cs
+++ STP_Task1_WinForms/STP_Task1/Form1.cs
@@ -18,73 +18,10 @@ namespace STP_Task1
             InitializeComponent();
         }
 
-        private long factorial(int value)
-        {
-            if (value == 0)
-            {
-                return 1;
-            }
-            if (value != 1)
-            {
-                return value * factorial(value - 1);
-            }
-            else
-            {
-                return 1;
-            }
-        }
-
-
-        private double MyCos(double value, int n)
-        {
-            double summ = 1;
-
-            int minus = 1;
-
-            double rowMember = 0;
-
-
-            for (int i = 1; i < n; i++)
-            {
-                rowMember = Math.Pow(value, 2 * i) / factorial(2 * i);
-                minus *= -1;
-                summ += minus * rowMember;
-            }
-
-            return summ;
-        }
-
-        private int computeIterationsForAccuracy(double epsilon, double value)
-        {
-            double summ = 1;
-
-            int minus = 1;
-
-            double rowMember = 0;
-
-            int iterator = 0;
-
-            double delta = 100000;
-
-            for (int i = 1; delta > epsilon; i++)
-            {
-                rowMember = Math.Pow(value, 2 * i) / factorial(2 * i);
-                minus *= -1;
-                summ += minus * rowMember;
-
-                delta = Math.Abs(Math.Cos(value) - summ);
-                iterator++;
-            }
-
-
-
-            return iterator;
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox4.Text = $"MyCos({textBox2.Text}) = " + 
-                MyCos(double.Parse(textBox2.Text, CultureInfo.InvariantCulture), 
+            textBox4.Text = $"MyCos({textBox2.Text}) = " +
+                MyMath.MyCos(double.Parse(textBox2.Text, CultureInfo.InvariantCulture), 
                 int.Parse(textBox6.Text));
             textBox5.Text = $"Envirement Cos({textBox2.Text}) = " + 
                 Math.Cos(double.Parse(textBox2.Text,
@@ -94,7 +31,7 @@ namespace STP_Task1
         private void button2_Click(object sender, EventArgs e)
         {
 
-            int iters = computeIterationsForAccuracy(
+            int iters = MyMath.computeIterationsForAccuracy(
                 double.Parse(textBox1.Text, CultureInfo.InvariantCulture), 
                 double.Parse(textBox7.Text, CultureInfo.InvariantCulture));
 
---
+++ STP_Task1_WinForms/STP_Task1/MyMath.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STP_Task1
+{
+    public class MyMath
+    {
+
+        public static long factorial(int value)
+        {
+            if (value == 0)
+            {
+                return 1;
+            }
+            if (value != 1)
+            {
+                return value * factorial(value - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+
+        public static double MyCos(double value, int n)
+        {
+            double summ = 1;
+
+            int minus = 1;
+
+            double rowMember = 0;
+
+            long fact = 2; // 0! + 2!
+
+
+            for (int i=1, f = 1; i < n; i++, f+=2)
+            {
+                Console.WriteLine($"{fact} <> {factorial(2*i)}\n");
+
+                rowMember = Math.Pow(value, 2*i) / fact;
+                minus *= -1;
+
+                fact *= (f+2)*(f+3);
+                summ += minus * rowMember;
+            }
+
+            return summ;
+        }
+
+        public static int computeIterationsForAccuracy(double epsilon, double value)
+        {
+            double summ = 1;
+            int minus = 1;
+            double rowMember = 0;
+            int iterator = 0;
+
+            double delta = 1;
+
+            long fact = 2;
+
+            for (int i = 1, f = 1; delta > epsilon; i++, f += 2)
+            {
+                rowMember = Math.Pow(value, 2*i) / fact;
+                minus *= -1;
+                fact *= (f + 2) * (f + 3);
+                summ += minus * rowMember;
+
+                Console.WriteLine(summ + " <> " + Math.Cos(value));
+
+                delta = Math.Abs(Math.Cos(value) - summ);
+                iterator++;
+            }
+
+            return iterator;
+        }
+
+    }
+}
