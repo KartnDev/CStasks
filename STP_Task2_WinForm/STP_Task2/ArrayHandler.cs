2106be214117ab9fc2fa51852c654d40c400517d
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-10-05 14:27:45 +0300

    Add files via upload

---
+++ STP_Task2_WinForm/STP_Task2/ArrayHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STP_Task2
+{
+    class ArrayHandler
+    {
+        public int[] Array { get; private set; } 
+
+        public ArrayHandler(int[] arrayArg)
+        {
+            this.Array = arrayArg;
+        }
+
+        public bool CheckSequence()
+        {
+            if(Array.Last() == 0)
+            {
+                return false;
+            }
+
+            for(int i = 0; i < Array.Length - 1; i++)
+            {
+                if((Array[i] > 0 && Array[i+1] > 0 ) || (Array[i] < 0 && Array[i + 1] < 0) || Array[i] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ STP_Task2_WinForm/STP_Task2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STP_Task2
+{
+    public partial class Form1 : Form
+    {
+        private static int[] RowArray;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckArrayButton_Click(object sender, EventArgs e)
+        {
+            ArrayHandler arrayHandler = new ArrayHandler(RowArray);
+            textBox2.Text = "" + arrayHandler.CheckSequence();
+            //textBox2.Text = "" + (arrayHandler.CheckSequence() ? 1 : 0);
+        }
+
+        private void GetArrayButton_Click(object sender, EventArgs e)
+        {
+            RowArray = new int[dataGridView1.RowCount - 1];
+
+
+            int iter = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value != null)
+                {
+                    RowArray[iter] = int.Parse((string)row.Cells[0].Value);
+                    iter++;
+                }
+            }
+
+            // мы имеет n+1 column 
+            // и в н+1 лежит разерервированный null
+        }
+    }
+}
