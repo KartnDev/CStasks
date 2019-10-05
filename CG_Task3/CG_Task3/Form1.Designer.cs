5a532a5fef6a5e537efbe347ea672e5c3b2a5ac4
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-10-05 16:10:22 +0300

    Add files via upload

---
+++ CG_Task3/CG_Task3/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace CG_Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DataGridArray = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GetArrayButton = new System.Windows.Forms.Button();
+            this.ResultButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DataGridArray});
+            this.dataGridView1.Location = new System.Drawing.Point(675, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(113, 214);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // DataGridArray
+            // 
+            this.DataGridArray.Frozen = true;
+            this.DataGridArray.HeaderText = "Array:";
+            this.DataGridArray.Name = "DataGridArray";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 425);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Canvas";
+            // 
+            // GetArrayButton
+            // 
+            this.GetArrayButton.Location = new System.Drawing.Point(675, 232);
+            this.GetArrayButton.Name = "GetArrayButton";
+            this.GetArrayButton.Size = new System.Drawing.Size(113, 23);
+            this.GetArrayButton.TabIndex = 3;
+            this.GetArrayButton.Text = "GetArray";
+            this.GetArrayButton.UseVisualStyleBackColor = true;
+            this.GetArrayButton.Click += new System.EventHandler(this.GetArrayButton_Click);
+            // 
+            // ResultButton
+            // 
+            this.ResultButton.Location = new System.Drawing.Point(675, 261);
+            this.ResultButton.Name = "ResultButton";
+            this.ResultButton.Size = new System.Drawing.Size(113, 23);
+            this.ResultButton.TabIndex = 4;
+            this.ResultButton.Text = "Result";
+            this.ResultButton.UseVisualStyleBackColor = true;
+            this.ResultButton.Click += new System.EventHandler(this.ResultButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(675, 290);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(113, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.ResultButton);
+            this.Controls.Add(this.GetArrayButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataGridArray;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GetArrayButton;
+        private System.Windows.Forms.Button ResultButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ CG_Task3/CG_Task3/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CG_Task3
+{
+    public partial class Form1 : Form
+    {
+        private int[] RowArray;
+        Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Column Diagram");
+            comboBox1.Items.Add("Circle Diagram");
+        }
+
+        private Color RandColor(int delim)
+        {
+            int r = (new Random(delim)).Next(0, 255);
+            int g = (new Random(171*delim)).Next(0, 255);
+            int b = (new Random(314*delim)).Next(0, 255);
+            return Color.FromArgb(r, g, b);
+        }
+
+
+        private void ResultButton_Click(object sender, EventArgs e)
+        {
+            graphics = pictureBox1.CreateGraphics();
+            DrawColumnDiagram(graphics, RowArray, 30, 200, 60, 40);
+        }
+
+        private void DrawColumnDiagram(Graphics g, int[] array, int startPosXArg, int startPosYArg, int ColWidth, int ColHeight)
+        {
+            int CorrentX = startPosXArg;
+            int CorrentY = startPosYArg;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                g.FillRectangle
+                    (new SolidBrush(RandColor(i)),
+                    CorrentX,
+                    CorrentY - ColHeight * array[i] ,
+                    ColWidth,
+                    CorrentY + ColHeight * array[i]
+                    );
+
+                CorrentX += ColWidth;
+            }
+        }
+
+        private void DrawCircleDiagram(Graphics g, int[] arrayArgs)
+        {
+
+        }
+
+
+        private void GetArrayButton_Click(object sender, EventArgs e)
+        {
+            RowArray = new int[dataGridView1.RowCount - 1];
+
+            int iter = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[0].Value != null)
+                {
+                    RowArray[iter] = int.Parse((string)row.Cells[0].Value);
+                    iter++;
+                }
+            }
+
+        }
+    }
+}
