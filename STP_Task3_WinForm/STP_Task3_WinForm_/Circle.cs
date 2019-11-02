72eb47c9cade639a2e0e95e4659b818e8973ee63
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-11-02 21:15:04 +0300

    Add files via upload

---
+++ STP_Task3_WinForm/STP_Task3_WinForm_/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STP_Task3_WinForm_
+{
+    public class Circle
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Radius { get; private set; }
+        public Circle(double x, double y, double radius)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Radius = radius;
+        }
+    }
+
+
+}
---
+++ STP_Task3_WinForm/STP_Task3_WinForm_/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace STP_Task3_WinForm_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Radius = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FeedButton = new System.Windows.Forms.Button();
+            this.ComputeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y,
+            this.Radius});
+            this.dataGridView1.Location = new System.Drawing.Point(567, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(212, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.Width = 50;
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            this.Y.Width = 50;
+            // 
+            // Radius
+            // 
+            this.Radius.HeaderText = "Radius";
+            this.Radius.Name = "Radius";
+            this.Radius.Width = 50;
+            // 
+            // FeedButton
+            // 
+            this.FeedButton.Location = new System.Drawing.Point(457, 12);
+            this.FeedButton.Name = "FeedButton";
+            this.FeedButton.Size = new System.Drawing.Size(104, 39);
+            this.FeedButton.TabIndex = 1;
+            this.FeedButton.Text = "FeedData";
+            this.FeedButton.UseVisualStyleBackColor = true;
+            this.FeedButton.Click += new System.EventHandler(this.FeedButton_Click);
+            // 
+            // ComputeButton
+            // 
+            this.ComputeButton.Location = new System.Drawing.Point(457, 57);
+            this.ComputeButton.Name = "ComputeButton";
+            this.ComputeButton.Size = new System.Drawing.Size(104, 39);
+            this.ComputeButton.TabIndex = 2;
+            this.ComputeButton.Text = "Compute";
+            this.ComputeButton.UseVisualStyleBackColor = true;
+            this.ComputeButton.Click += new System.EventHandler(this.ComputeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 450);
+            this.Controls.Add(this.ComputeButton);
+            this.Controls.Add(this.FeedButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Radius;
+        private System.Windows.Forms.Button FeedButton;
+        private System.Windows.Forms.Button ComputeButton;
+    }
+}
+
