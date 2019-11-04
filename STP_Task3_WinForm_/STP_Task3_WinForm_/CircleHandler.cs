c3ad814a0f3ff39ae99e938f23d5f3e17364afac
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-11-04 15:05:17 +0300

    Add files via upload

---
+++ STP_Task3_WinForm_/STP_Task3_WinForm_/CircleHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STP_Task3_WinForm_
+{
+    public class CircleHandler
+    {
+        private List<Circle> circles;
+        private readonly int HEIGHT;
+        private readonly int WIDTH;
+        public CircleHandler(IEnumerable<Circle> circles, int height, int width)
+        {
+            this.circles = circles.ToList();
+            this.HEIGHT = height;
+            this.WIDTH = width;
+        }
+
+        public KeyValuePair<int, int> FirstPointOrDefualt()
+        {
+            for (int i = 0; i < HEIGHT; i++)
+            {
+                for (int j = 0; j < WIDTH; j++)
+                {
+                    if(IsPointHaveInAllC(i, j))
+                    {
+                        return new KeyValuePair<int, int>(i, j);
+                    }
+                }
+            }
+            return new KeyValuePair<int, int>(-1, -1);
+            //return defualt(KeyValuePair<int, int>;
+        }
+
+        private bool IsPointHaveInAllC(int x, int y)
+        {
+            int flag = 0;
+            foreach (var circle in circles)
+            {
+                if(IntersentPoint(circle, x, y))
+                {
+                    flag++;
+                }
+            }
+            return flag == circles.Count;
+        }
+
+        public static bool IntersentPoint(Circle correntCircle, double pointPosX, double pointPosY)
+        {
+            double distanceBetweenCenterAndPoint = Math.Sqrt(
+                (correntCircle.X - pointPosX) * (correntCircle.X - pointPosX) +
+                (correntCircle.Y - pointPosY) * (correntCircle.Y - pointPosY));
+            return correntCircle.Radius > distanceBetweenCenterAndPoint ? true : false;
+        }
+
+
+
+    }
+}
---
+++ STP_Task3_WinForm_/STP_Task3_WinForm_/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STP_Task3_WinForm_
+{
+
+    public partial class Form1 : Form
+    {
+        List<Circle> circles = new List<Circle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FeedButton_Click(object sender, EventArgs e)
+        {
+            int iter = 1;
+
+            foreach(DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Index == dataGridView1.RowCount - 1)
+                {
+                    break;
+                }
+                double dataRaduis, dataX, dataY;
+
+                if (double.TryParse(row.Cells[0].Value.ToString(), out dataX) &&
+                   double.TryParse(row.Cells[1].Value.ToString(), out dataY) &&
+                   double.TryParse(row.Cells[2].Value.ToString(), out dataRaduis))
+                {
+                    circles.Add(new Circle(dataX, dataY, dataRaduis));
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный формат на строке: " + iter);
+                    return;
+                }
+                iter++;
+            }
+            
+        }
+
+        private void ComputeButton_Click(object sender, EventArgs e)
+        {
+            CircleHandler circleHandler = new CircleHandler(circles, 1000, 1000);
+
+            KeyValuePair<int, int> coord = circleHandler.FirstPointOrDefualt();
+
+            ResultOutputLable.Text = coord.Key == -1 && coord.Value == -1 ? "Таких нет" : $"Point (X: {coord.Key}, Y: {coord.Value})";
+
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ResultOutputLable.Text = "...";
+            circles.Clear();
+        }
+    }
+}
