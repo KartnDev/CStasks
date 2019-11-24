dc02cac3d3107a148cc206e7c13814b7b3a71fca
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-11-24 15:02:56 +0300

    Add files via upload

--- CG_Task5/CG_Task5/Form1.cs
+++ CG_Task5/CG_Task5/Form1.cs
@@ -16,12 +16,6 @@ namespace CG_Task5
         private Brush MyBrush { get; set; }
         private Pen MyPen { get; set; }
 
-
-        //private int height_z = 200;
-        //private int width_y = 200;
-        //private int len_x = 200;
-        //private int x0 = 120;
-        //private int y0 = 120;
         public Form1()
         {
             InitializeComponent();
@@ -33,9 +27,127 @@ namespace CG_Task5
         private void RenderButton_Click(object sender, EventArgs e)
         {
             //Render2DRotationZ(250, 250, 70);
-            drawCube(250, 250, 70, 50, "OX");
+            //drawCube(250, 250, 70, 50, "OX");
+            //RenderTetrahedron(Graphics3D, MyPen, 100, 100, 200, 300, Math.PI / 4);
+            //RenderOctahedron(Graphics3D, MyPen, 300, 300, 70);
+            DrawDodecahedron(Graphics3D, MyPen, 400, 400, 50);
         }
 
+
+        private void RenderTetrahedron(Graphics g, Pen pen, int x, int y, int width, int height, double alpha)
+        {
+            g.DrawLine(MyPen, x, y, x + width, y);
+            g.DrawLine(MyPen, x, y, (float)((x + width) / 1.5), (float)(width * Math.Cos(alpha)));
+            g.DrawLine(MyPen, x + width, y, (float)((x + width) / 1.5), (float)(width * Math.Sin(alpha)));
+
+            g.DrawLine(MyPen, x + width, y, (float)((x + width) / 1.5), y+height);
+            g.DrawLine(MyPen, x, y, (float)((x + width) / 1.5), y + height);
+            g.DrawLine(MyPen, (float)((x + width) / 1.5), (float)(width * Math.Sin(alpha)), (float)((x + width) / 1.5), y + height);
+        }
+
+        private void RenderOctahedron(Graphics g, Pen pen, int x, int y, int radius)
+        {
+            
+            
+
+
+            for (double i = 0; true; i += Math.PI / 10)
+            {
+                //draw AXIS
+                Graphics3D.DrawLine(new Pen(MyBrush, 2), x, (y - radius), x, y + radius);
+                Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
+
+                for (double alpha = 0; alpha < 2 * Math.PI; alpha += Math.PI / 2)
+                {
+                    
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha)),
+                                               (int)(x + radius * Math.Cos(i + alpha + Math.PI / 2) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha + Math.PI / 2)));
+
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha)),
+                                               (int)x, 
+                                               (int)(y - radius));
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha)),
+                                               (int)x,
+                                               (int)(y + radius));
+                }
+
+                ClearAndSleep(Graphics3D);
+            }
+
+        }
+
+
+        private void DrawDodecahedron(Graphics g, Pen pen, int x, int y, int radius)
+        {
+            for (double i = 0; true; i += Math.PI / 30)
+            {
+
+                //draw AXIS
+                Graphics3D.DrawLine(new Pen(MyBrush, 2), x, (y - 150), x, y);
+
+
+                //Render Last small traingle
+                for (double alpha = 0; alpha < 2 * Math.PI; alpha += 2*Math.PI / 3)
+                {
+                    Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha)),
+                                               (int)(x + radius * Math.Cos(i + alpha + 2*Math.PI / 3) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha + 2 * Math.PI / 3)));
+                }
+
+
+                //Render pre-last big traingle + No-offset
+                for (double alpha = Math.PI / 3; alpha < 2 * Math.PI + Math.PI / 3; alpha += 2 * Math.PI / 3)
+                {
+                    Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
+                    Graphics3D.DrawLine(MyPen, (int)(x + 2 * radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y - 50 + 2 * radius * Math.Sin(i + alpha)),
+                                               (int)(x + 2 * radius * Math.Cos(i + alpha + 2 * Math.PI / 3) * 2),
+                                               (int)(y - 50 + 2 * radius * Math.Sin(i + alpha + 2 * Math.PI / 3)));
+                }
+                /*
+
+                // Glue last and pre-last triangles
+                for (double alpha = 0; alpha < 2 * Math.PI + Math.PI / 3; alpha += 2 * Math.PI / 3)
+                {
+                    Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y + radius * Math.Sin(i + alpha)),
+                                               (int)(x + 2 * radius * Math.Cos(i + alpha + 2 * Math.PI / 3) * 2),
+                                               (int)(y - 50 + 2 * radius * Math.Sin(i + alpha + 2 * Math.PI / 3)));
+                }*/
+
+                //Render pre-First big traingle + No-offset
+                for (double alpha = 0; alpha < 2 * Math.PI; alpha += 2 * Math.PI / 3)
+                {
+                    Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
+                    Graphics3D.DrawLine(MyPen, (int)(x + 2*radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y - 100 + 2*radius * Math.Sin(i + alpha)),
+                                               (int)(x + 2*radius * Math.Cos(i + alpha + 2 * Math.PI / 3) * 2),
+                                               (int)(y - 100 + 2*radius * Math.Sin(i + alpha + 2 * Math.PI / 3)));
+                }
+                //Render First small traingle + offset
+                for (double alpha = Math.PI / 3; alpha < 2 * Math.PI + Math.PI / 3; alpha += 2 * Math.PI / 3)
+                {
+                    Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
+                                               (int)(y - 150 + radius * Math.Sin(i + alpha)),
+                                               (int)(x + radius * Math.Cos(i + alpha + +2 * Math.PI / 3) * 2),
+                                               (int)(y - 150 + radius * Math.Sin(i + alpha + +2 * Math.PI / 3)));
+                }
+
+
+
+
+
+                ClearAndSleep(Graphics3D);
+            }
+        }
 
 
         private void Render2DRotationZ(int x, int y, int radius)
@@ -45,9 +157,9 @@ namespace CG_Task5
                 for (double alpha = 0; alpha < 2 * Math.PI; alpha += Math.PI / 2)
                 {
                     Graphics3D.DrawRectangle(MyPen, x, y, 2, 2);
-                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha)),
+                    Graphics3D.DrawLine(MyPen, (int)(x + radius * Math.Cos(i + alpha) * 2),
                                                (int)(y + radius * Math.Sin(i + alpha)),
-                                               (int)(x + radius * Math.Cos(i + alpha + Math.PI / 2)),
+                                               (int)(x + radius * Math.Cos(i + alpha + Math.PI / 2) * 2),
                                                (int)(y + radius * Math.Sin(i + alpha + Math.PI / 2)));
                 }
 
@@ -56,10 +168,10 @@ namespace CG_Task5
         }
 
 
-        private void drawCube(int x0, int y0, int radius, int height_z, string rotationDim = "OY")
+        private void drawCube(int x0, int y0, int radius, int height_z, string rotationAxis = "OY")
         {
 
-            if (rotationDim == "OZ")
+            if (rotationAxis == "OZ")
             {
                 for (double i = 0; true; i += Math.PI / 10)
                 {
@@ -90,7 +202,7 @@ namespace CG_Task5
                 }
 
             }
-            else if (rotationDim == "OX")
+            else if (rotationAxis == "OX")
             {
                 for (double i = 0; true; i += Math.PI / 10)
                 {
@@ -120,7 +232,7 @@ namespace CG_Task5
                     ClearAndSleep(Graphics3D);
                 }
             }
-            else if (rotationDim == "OY")
+            else if (rotationAxis == "OY")
             {
 
             }
