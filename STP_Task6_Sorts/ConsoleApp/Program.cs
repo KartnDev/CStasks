c85ee39d71190419cdad04eaa5b03636ec2b1949
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-12-16 18:25:57 +0300

    Add files via upload

---
+++ STP_Task6_Sorts/ConsoleApp/Program.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp
+{
+    static class Sorts
+    {
+        public delegate int CompareDelegate<TValue>(TValue item0, TValue item1);
+        public static IEnumerable<T> QuickSort<T>(this IEnumerable<T> list) where T : IComparable<T>
+        {
+            if (!list.Any())
+            {
+                return Enumerable.Empty<T>();
+            }
+            var pivot = list.First();
+            var smaller = list.Skip(1).Where(item => item.CompareTo(pivot) <= 0).QuickSort();
+            var larger = list.Skip(1).Where(item => item.CompareTo(pivot) > 0).QuickSort();
+
+            return smaller.Concat(new[] { pivot }).Concat(larger);
+        }
+
+        public static IEnumerable<TValue> QuickSortWithDelegate<TValue>
+            (this IEnumerable<TValue> list, CompareDelegate<TValue> compareDelegate)
+        {
+            if (!list.Any())
+            {
+                return Enumerable.Empty<TValue>();
+            }
+            var pivot = list.First();
+            var smaller = list.Skip(1).Where(item => compareDelegate(item, pivot) <= 0).QuickSortWithDelegate(compareDelegate);
+            var larger = list.Skip(1).Where(item => compareDelegate(item, pivot) > 0).QuickSortWithDelegate(compareDelegate);
+
+            return smaller.Concat(new[] { pivot }).Concat(larger);
+        }
+
+
+
+        public static IEnumerable<TValue> LinqQuickSort<TValue>(this IEnumerable<TValue> list) where TValue : IComparable<TValue>
+        {
+            return !list.Any() ? Enumerable.Empty<TValue>() : list.Skip(1).Where(
+                item => item.CompareTo(list.First()) <= 0).LinqQuickSort().Concat(new[] { list.First() })
+                    .Concat(list.Skip(1).Where(item => item.CompareTo(list.First()) > 0).LinqQuickSort());
+        }
+
+        public static IEnumerable<TValue> BubbleSort<TValue>(this IEnumerable<TValue> item) where TValue : IComparable<TValue>
+        {
+            int i, j;
+            TValue temp;
+            var array = item.ToArray();
+            for (i = array.Count() - 1; i > 0; i--)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public static IEnumerable<TValue> BubbleSortWithDelegate<TValue>(this IEnumerable<TValue> item, CompareDelegate<TValue> compareDelegate)
+        {
+            int i, j;
+            TValue temp;
+            var array = item.ToArray();
+            for (i = array.Count() - 1; i > 0; i--)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    if (compareDelegate(array[j], array[j + 1]) > 0)
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public static IEnumerable<TValue> SelectionSort<TValue>(this IEnumerable<TValue> item) where TValue : IComparable<TValue>
+        {
+            var list = item.ToList();
+            for (int i = 0; i < list.Count() - 1; ++i)
+            {
+                TValue minElement = list[i];
+                int minLocation = i;
+
+                for (int j = i + 1; j < list.Count(); ++j)
+                {
+                    if (list[j].CompareTo(minElement) < 0)
+                    {
+                        minElement = list[j];
+                        minLocation = j;
+                    }
+                }
+
+                if (minLocation != i)
+                {
+                    TValue temp = list[minLocation];
+                    list[minLocation] = list[i];
+                    list[i] = temp;
+                }
+            }
+            return list;
+        }
+        public static IEnumerable<TValue> SelectionSortWithDelegate<TValue>(this IEnumerable<TValue> item, CompareDelegate<TValue> compareDelegate)
+        {
+            var list = item.ToList();
+            for (int i = 0; i < list.Count() - 1; ++i)
+            {
+                TValue minElement = list[i];
+                int minLocation = i;
+
+                for (int j = i + 1; j < list.Count(); ++j)
+                {
+                    if (compareDelegate(list[j], minElement) < 0)
+                    {
+                        minElement = list[j];
+                        minLocation = j;
+                    }
+                }
+
+                if (minLocation != i)
+                {
+                    TValue temp = list[minLocation];
+                    list[minLocation] = list[i];
+                    list[i] = temp;
+                }
+            }
+            return list;
+        }
+
+        public static IEnumerable<TValue> InsertSort<TValue>(this IEnumerable<TValue> item) where TValue : IComparable<TValue>
+        {
+            int i, j;
+
+            var list = item.ToList();
+
+            for (i = 1; i < list.Count(); i++)
+            {
+                TValue value = list[i];
+                j = i - 1;
+                while ((j >= 0) && (list[j].CompareTo(value) > 0))
+                {
+                    list[j + 1] = list[j];
+                    j = j - 1;
+                }
+                list[j + 1] = value;
+            }
+            return list;
+        }
+        public static IEnumerable<TValue> InsertSortWithDelegate<TValue>(this IEnumerable<TValue> item, CompareDelegate<TValue> compareDelegate)
+        {
+            int i, j;
+
+            var list = item.ToList();
+
+            for (i = 1; i < list.Count(); i++)
+            {
+                TValue value = list[i];
+                j = i - 1;
+                while ((j >= 0) && (compareDelegate(list[j], value) > 0))
+                {
+                    list[j + 1] = list[j];
+                    j = j - 1;
+                }
+                list[j + 1] = value;
+            }
+            return list;
+        }
+        public static IEnumerable<TValue> ShellSort<TValue>(this IEnumerable<TValue> item) where TValue : IComparable<TValue>
+        {
+            var list = item.ToList();
+
+            int h = 1;
+
+            while (h < (list.Count >> 1))
+            {
+                h = (h << 1) + 1;
+            }
+
+            while (h >= 1)
+            {
+                for (int i = h; i < list.Count; i++)
+                {
+                    int k = i - h;
+                    for (int j = i; j >= h && list[j].CompareTo(list[k]) < 0; k -= h)
+                    {
+                        TValue temp = list[j];
+                        list[j] = list[k];
+                        list[k] = temp;
+                        j = k;
+                    }
+                }
+                h >>= 1;
+            }
+            return list;
+        }
+
+
+        public static IEnumerable<TValue> ShellSortWithDelegate<TValue>(this IEnumerable<TValue> item, CompareDelegate<TValue> compareDelegate)
+        {
+            var list = item.ToList();
+
+            int h = 1;
+
+            while (h < (list.Count >> 1))
+            {
+                h = (h << 1) + 1;
+            }
+
+            while (h >= 1)
+            {
+                for (int i = h; i < list.Count; i++)
+                {
+                    int k = i - h;
+                    for (int j = i; j >= h && compareDelegate(list[j], list[k]) < 0; k -= h)
+                    {
+                        TValue temp = list[j];
+                        list[j] = list[k];
+                        list[k] = temp;
+                        j = k;
+                    }
+                }
+                h >>= 1;
+            }
+            return list;
+        }
+    }
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string[] a = { "dfd", "sss", "aaa", "bba", "baa", "bbb" };
+
+
+            //var b = Sorts.QuickSortWithDelegate(a, (string item1, string item2) => string.Compare(item1, item2));
+            //b = a.QuickSortWithDelegate((string item1, string item2) => string.Compare(item1, item2));
+            //var b = a.BubbleSort();
+            //var b = a.BubbleSortWithDelegate((string item1, string item2) => string.Compare(item1, item2));
+            //var b = a.SelectionSort();
+            //var b = a.SelectionSortWithDelegate((string item1, string item2) => string.Compare(item1, item2));
+            //var b = a.InsertSort();
+            //var b = a.InsertSortWithDelegate((string item1, string item2) => string.Compare(item1, item2));
+            //var b = a.ShellSort();
+            var b = a.ShellSortWithDelegate((string item1, string item2) => string.Compare(item1, item2));
+            foreach(var item in b)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ STP_Task6_Sorts/STP_Task6_Sorts/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("STP_Task6_Sorts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("STP_Task6_Sorts")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("97f79a2c-e3e4-4b17-9f15-75e8239662e4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
