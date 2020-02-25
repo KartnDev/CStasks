585650d1ecec38a1d7b66410e066f8d5b01023ce
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-02-25 22:56:18 +0300

    Add files via upload

---
+++ SecSmTask1/SecSmTask1/Collections/Generics/DoubleLinkedList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Collections.Generics
+{
+    public class DoubleLinkedList<TValue> : IList<TValue>
+    {
+
+        private Node headPtr = null;
+        private Node tailPtr = null;
+
+        private int lenght = 0;
+
+        public DoubleLinkedList()
+        {
+
+        }
+
+        public DoubleLinkedList(DoubleLinkedList<TValue> list)
+        {
+
+        }
+
+
+        public TValue this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => lenght;
+
+        public TValue First { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public TValue Last { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int AddFirst(TValue value)
+        {
+            if(lenght == 0)
+            {
+                headPtr = tailPtr = new Node(value);
+            }
+            else
+            {
+                headPtr = new Node(value, null, headPtr); 
+            }
+            lenght++;
+            return 1;
+        }
+
+        public int AddLast(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue ElementAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAt(int index, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetElementAt(TValue data, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Node
+        {
+            public Node(TValue data, Node prev = null, Node next = null)
+            {
+                this.data = data;
+                this.nextPrt = next;
+                this.prevPtr = prev;
+            }
+
+            public Node prevPtr;
+            public Node nextPrt;
+            public TValue data;
+        }
+
+
+    }
+}
---
+++ SecSmTask1/SecSmTask1/Collections/Generics/IList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Collections.Generics
+{
+    public interface IList<TValue> : IEnumerable<TValue>
+    {
+        int Count { get; }
+        TValue First { get; set; }
+        TValue Last { get; set; }
+        TValue this[int index]
+        {
+            get;
+            set;
+        }
+
+        TValue ElementAt(int index);
+        void SetElementAt(TValue data, int index);
+
+        int AddFirst(TValue value);
+        int AddLast(TValue value);
+
+        void InsertAt(int index, TValue value);
+        void Remove(TValue value);
+        void RemoveAt(int index);
+
+        bool Contains(TValue value);
+
+        int IndexOf(TValue value);
+        void Clear();
+    }
+}
