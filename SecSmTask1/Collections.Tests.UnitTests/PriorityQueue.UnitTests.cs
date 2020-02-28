d6cf305c6dae831e9d6f39922131b699eab89446
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-02-28 20:54:43 +0300

    task1 tests

---
+++ SecSmTask1/Collections.Tests.UnitTests/PriorityQueue.UnitTests.cs
@@ -0,0 +1,90 @@
+﻿using Collections.Collections.Generics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Tests.UnitTests
+{
+    static class StringHelper
+    {
+        public static string ReverseString(this string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+
+
+    [TestClass]
+    public class PriorityQueueTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            PriorityQueue<string> queue = new PriorityQueue<string>((value1, value2) => string.Compare(value1, value2));
+
+            foreach (var value in "fgxyumnqrstvabwhijkopcdelz")
+            {
+                queue.Add(value.ToString());
+            }
+            Assert.AreEqual("fgxyumnqrstvabwhijkopcdelz".Length, queue.Count);
+            foreach (var value in "abcdefghijklmnopqrstuvwxyz".ReverseString())
+            {
+                Assert.AreEqual(value, queue.Remove());
+            }
+
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            PriorityQueue<string> queue = new PriorityQueue<string>((value1, value2) => string.Compare(value1, value2));
+
+            foreach (var value in "fgxyumnqrstvabwhijkopcdelz")
+            {
+                queue.Add(value.ToString());
+            }
+            foreach (var value in "abcdefghijklmnopqrstuvwxyz".ReverseString())
+            {
+                Assert.AreEqual(value, queue.Remove());
+            }
+            Assert.AreEqual(0, queue.Count);
+            Assert.ThrowsException<InvalidOperationException>(() => queue.Remove());
+        }
+
+        [TestMethod]
+        public void CountTest()
+        {
+            PriorityQueue<string> queue = new PriorityQueue<string>((value1, value2) => string.Compare(value1, value2));
+
+            for (int i = 0; i < 100; i++)
+            {
+                queue.Add(i.ToString());
+            }
+            Assert.AreEqual(100, queue.Count);
+            for (int i = 0; i < 100; i++)
+            {
+                queue.Remove();
+            }
+            Assert.AreEqual(0, queue.Count);
+        }
+        [TestMethod]
+        public void TopTest()
+        {
+            PriorityQueue<string> queue = new PriorityQueue<string>((value1, value2) => string.Compare(value1, value2));
+
+
+            queue.Add("ABC");
+            Assert.AreEqual("ABC", queue.TopItem);
+
+            queue.Add("ADA");
+            Assert.AreEqual("ADA", queue.TopItem);
+            queue.Add("AAA");
+            Assert.AreEqual("ADA", queue.TopItem);
+        }
+    }
+}
---
+++ SecSmTask1/Collections.Tests.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Collections.Tests.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Collections.Tests.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8bac3017-6835-4f2e-85c7-f370940da87b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
