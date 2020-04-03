cab5e67639d29a357f16ff7b26aa6f956317ebcd
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-04-03 16:14:12 +0300

    planned attributes route map

---
+++ SecSemTask2_WebServer/SecSemTask2_WebServer/WebServer/Core/Routers/HttpMethodTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecSemTask2_WebServer.WebServer.Core.Routers
+{
+    public enum HttpMethodTypes
+    {
+        HttpGet,
+        HttpPost,
+        HttpServerError,
+        HttpClientError
+    }
+}
