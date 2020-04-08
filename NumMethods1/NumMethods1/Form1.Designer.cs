2a53a5383e86ffdc7a92a3ca12a30b092e6aeb38
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-04-08 17:27:25 +0300

    fixed commit

--- NumMethods1/NumMethods1/Form1.Designer.cs
+++ NumMethods1/NumMethods1/Form1.Designer.cs
@@ -31,7 +31,10 @@
             this.canvasBox = new System.Windows.Forms.PictureBox();
             this.canvasTextlabel = new System.Windows.Forms.Label();
             this.renderButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.canvasBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // canvasBox
@@ -63,17 +66,42 @@
             this.renderButton.UseVisualStyleBackColor = true;
             this.renderButton.Click += new System.EventHandler(this.RenderButtonOnClick);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lagruange",
+            "Newton"});
+            this.comboBox1.Location = new System.Drawing.Point(690, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(690, 104);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(98, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 16;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.renderButton);
             this.Controls.Add(this.canvasTextlabel);
             this.Controls.Add(this.canvasBox);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.canvasBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,6 +112,8 @@
         private System.Windows.Forms.PictureBox canvasBox;
         private System.Windows.Forms.Label canvasTextlabel;
         private System.Windows.Forms.Button renderButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
